9e5f7d8b27b4e5a245991a7243ddb7a5d6f9c9e4
ianvermeulen <iyv2@cornell.edu>
2015-11-23 00:36:48 -0500

    update to not use mallocs and use 1-D arrays for everything

--- neural_network/MNIST.cpp
+++ neural_network/MNIST.cpp
@@ -11,87 +11,79 @@
 #include "neuralNetwork.h"
 #include "MNISTParser.h"
 
+#define COLS 28
+#define ROWS 28
+
 int main() {
+
   NeuralNetwork* nn;
   srand((unsigned int)time(0));
 
-  int* training_label = (int*) malloc(TRAINING_SIZE * sizeof(int));
-  float** training_data = (float**) malloc(TRAINING_SIZE * sizeof(float*));
-  for (int i = 0; i < TRAINING_SIZE*10; i++)
-    training_data[i] = (float*) malloc(IMAGE_SIZE*sizeof(float));
+  int training_label[TRAINING_SIZE];
+  float training_data[TRAINING_SIZE * IMAGE_SIZE];
 
-  int* testing_label = (int*) malloc(TESTING_SIZE * sizeof(int));
-  float** testing_data = (float**) malloc(TESTING_SIZE * sizeof(float*));
-  for (int i = 0; i < TESTING_SIZE; i++)
-    testing_data[i] = (float*) malloc(IMAGE_SIZE*sizeof(float));
+  int testing_label[TESTING_SIZE];
+  float testing_data[TESTING_SIZE * IMAGE_SIZE];
 
   char inputfile[23];
 
   read_mnist(training_data, training_label, testing_data, testing_label);
 
-  std::cout << "files read\n";
-
   // Read input file for the testing set
   std::string line;
   std::ifstream myfile;
 
-
   // "shuffle" the data (it's not perfectly random but i don't care)
   int j1, j2, temp;
+  float temp_f;
   float* templ;
   for (int i = 0; i < 50000; i++) {
     j1 = rand() % (TRAINING_SIZE);
     j2 = rand() % (TRAINING_SIZE);
-    templ = training_data[j1];
     temp = training_label[j1];
-    training_data[j1] = training_data[j2];
     training_label[j1] = training_label[j2];
-    training_data[j2] = templ;
     training_label[j2] = temp;
+    for (int k = 0; k < IMAGE_SIZE; k++) {
+      temp_f = training_data[j1*IMAGE_SIZE + k];
+      training_data[j1*IMAGE_SIZE + k] = training_data[j2*IMAGE_SIZE + k];
+      training_data[j2*IMAGE_SIZE + k] = temp_f;
+    }
   }
   for (int i = 0; i < 5000; i++) {
     j1 = rand() % (TESTING_SIZE);
     j2 = rand() % (TESTING_SIZE);
-    templ = testing_data[j1];
     temp = testing_label[j1];
-    testing_data[j1] = testing_data[j2];
     testing_label[j1] = testing_label[j2];
-    testing_data[j2] = templ;
     testing_label[j2] = temp;
-  }
-
-  for (int i = 0; i < 10; i++) {
-    std::cout << "==============" << training_label[i] << "=============\n";
-    for (int j = 0; j < 28; j++) {
-      for (int k = 0; k < 28; k++) {
-        std::cout << training_data[i][28*j+k];
-        if (training_data[i][28*j+k] > 99)
-          std::cout << " ";
-        else if (training_data[i][28*j+k] > 9)
-          std::cout << "  ";
-        else
-          std::cout << "   ";
-      }
-      std::cout << "\n";
+    for (int k = 0; k < IMAGE_SIZE; k++) {
+      temp_f = testing_data[j1*IMAGE_SIZE + k];
+      testing_data[j1*IMAGE_SIZE + k] = testing_data[j2*IMAGE_SIZE + k];
+      testing_data[j2*IMAGE_SIZE + k] = temp_f;
     }
-    std::cout << "\n";
   }
 
-  nn = neuralNetwork(IMAGE_SIZE, 500, 10);
+  std::cout << "files read\n";
+
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << "==============" << training_label[i] << "=============\n";
+  //   for (int j = 0; j < ROWS; j++) {
+  //     for (int k = 0; k < COLS; k++) {
+  //       std::cout << training_data[i*IMAGE_SIZE + COLS*j+k];
+  //       if (training_data[i*IMAGE_SIZE + COLS*j+k] > 99)
+  //         std::cout << " ";
+  //       else if (training_data[i*IMAGE_SIZE + COLS*j+k] > 9)
+  //         std::cout << "  ";
+  //       else
+  //         std::cout << "   ";
+  //     }
+  //     std::cout << "\n";
+  //   }
+  //   std::cout << "\n";
+  // }
+
+  nn = neuralNetwork(IMAGE_SIZE, 10);
   trainNetwork(nn, training_data, training_label, TRAINING_SIZE,
                    testing_data, testing_label, TESTING_SIZE); 
-  destroyNeuralNetwork(nn);
-
-  free(training_label);
-  for (int i = 0; i < TRAINING_SIZE; i++)
-    free(training_data[i]);
-  free(training_data);
-
-  free(testing_label);
-  for (int i = 0; i < TESTING_SIZE; i++)
-    free(testing_data[i]);
-  free(testing_data);
 
   myfile.close();
-
 }
--- neural_network/neuralNetwork.cpp
+++ neural_network/neuralNetwork.cpp
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 
-#define learningRate 0.005
-#define momentum     0.9
+#define learningRate 0.02
+#define momentum     0.8
 #define maxEpoch     150
-//#define desiredAccuracy
+#define INPUTSIZE    784
+#define HIDDENSIZE    784
+#define OUTPUTSIZE    10
 
 typedef struct _NeuralNetwork {
-  // sizes of each layer
-  int nInput, nHidden, nOutput;
-
   // neuron values
-  float* input;
-  float* hidden;
-  float* output;
+  float input[INPUTSIZE+1];
+  float hidden[HIDDENSIZE+1];
+  float output[OUTPUTSIZE];
 
   // inter-layer weight matrices
-  float** weightIH;
-  float** weightHO;
+  float weightIH[(INPUTSIZE+1) * HIDDENSIZE];
+  float weightHO[(HIDDENSIZE+1) * OUTPUTSIZE];
   
   // changes to weights for backpropagation
-  float** deltaIH;
-  float** deltaHO;
+  float deltaIH[(INPUTSIZE+1) * HIDDENSIZE];
+  float deltaHO[(HIDDENSIZE+1) * OUTPUTSIZE];
 
   // error gradients for backpropagation
-  float* hiddenErrorGradients;
-  float* outputErrorGradients;
+  float hiddenErrorGradients[HIDDENSIZE];
+  float outputErrorGradients[OUTPUTSIZE];
 
   //accuracy stats per epoch
   float trainingSetAccuracy, testSetAccuracy;
@@ -40,86 +40,44 @@ typedef struct _NeuralNetwork {
 //  the '+1' in all the malloc's is to include an extra neuron whose
 //  value is set to -1. This works to set a bias value to each connection.
 //  the _<____+1 in the for loop conditions could be _<=____ instead
-NeuralNetwork* neuralNetwork(int nInput, int nHidden, int nOutput) {
+NeuralNetwork* neuralNetwork(int nInput, int nOutput) {
+  assert(nInput == INPUTSIZE);
+  assert(nOutput == OUTPUTSIZE);
+
   // set bounding variables for the initial weights
   float rangeIH = 1/sqrt((float)nInput);
-  float rangeHO = 1/sqrt((float)nHidden); 
+  float rangeHO = 1/sqrt((float)HIDDENSIZE); 
 
   // allocate memory for struct pointer
   NeuralNetwork* nn = (NeuralNetwork*) malloc(sizeof(NeuralNetwork));
-
-  // set layer sizes
-  nn->nInput = nInput;
-  nn->nHidden = nHidden;
-  nn->nOutput = nOutput;
   
-  // allocate memory for neuron values and error gradients
-  nn->input =  (float*) malloc((nInput+1) * sizeof(float));
-  nn->hidden = (float*) malloc((nHidden+1) * sizeof(float));
-  nn->hiddenErrorGradients = (float*) malloc((nHidden+1) * sizeof(float));
-  nn->output = (float*) malloc(nOutput * sizeof(float));
-  nn->outputErrorGradients = (float*) malloc(nOutput * sizeof(float));
-  
-  // allocate memory for 2-D IH weight arrays and intialize the input biases to 0
-  // also initialize the the IH weights to some constrained random values
-  nn->weightIH = (float**) malloc((nInput+1) * sizeof(float*));
-  nn->deltaIH = (float**) malloc((nInput+1) * sizeof(float*));
+  // initialize the the IH weights to some constrained random values
   for(int i = 0; i < nInput+1; i++) {   
     nn->input[i] = 0;
-    nn->weightIH[i] = (float*) malloc(nHidden * sizeof(float));
-    nn->deltaIH[i] = (float*) malloc(nHidden * sizeof(float));
-    for(int j = 0; j < nHidden; j++) {
-      nn->weightIH[i][j] = (((float)(rand()%100)+1)/100  * 2 * rangeIH) - rangeIH;
-      nn->deltaIH[i][j] = 0;      
+    for(int j = 0; j < HIDDENSIZE; j++) {
+      nn->weightIH[i*HIDDENSIZE + j] = (((float)(rand()%100)+1)/100  * 2 * rangeIH) - rangeIH;
+      nn->deltaIH[i*HIDDENSIZE + j] = 0;      
     }
   }
   nn->input[nInput] = -1;
 
-  // allocate memory for 2-D HO weight arrays and intialize the output biases to 0
-  // also initialize the the HO weights to some constrained random values
-  nn->weightHO = (float**) malloc((nHidden+1) * sizeof(float*));
-  nn->deltaHO = (float**) malloc((nHidden+1) * sizeof(float*));
-  for(int i = 0; i < nHidden+1; i++) {   
+  // initialize the the HO weights to some constrained random values
+  for(int i = 0; i < HIDDENSIZE+1; i++) {   
     nn->hidden[i] = 0;
-    nn->hiddenErrorGradients[i] = 0;
-    nn->weightHO[i] = (float*) malloc(nOutput * sizeof(float));
-    nn->deltaHO[i] = (float*) malloc(nOutput * sizeof(float));
+    if (i != HIDDENSIZE) nn->hiddenErrorGradients[i] = 0;
     for(int j = 0; j < nOutput; j++) {
-      nn->weightHO[i][j] = (((float)(rand()%100)+1)/100  * 2 * rangeHO) - rangeHO;      
-      nn->deltaHO[i][j] = 0; 
+      nn->weightHO[i*OUTPUTSIZE + j] = (((float)(rand()%100)+1)/100  * 2 * rangeHO) - rangeHO;      
+      nn->deltaHO[i*OUTPUTSIZE + j] = 0; 
     }
   }
-  nn->hidden[nHidden] = -1;
+  nn->hidden[HIDDENSIZE] = -1;
 
   for (int i=0; i < nOutput; i++) {
     nn->output[i] = 0;
-    
-    // TODO: Size of this array???????????????????????
     nn->outputErrorGradients[i] = 0;
   }
 
   return nn;
-}
-
-void destroyNeuralNetwork(NeuralNetwork* nn) {
-  free(nn->input);
-  free(nn->hidden);
-  free(nn->hiddenErrorGradients);
-  free(nn->output);
-  free(nn->outputErrorGradients);
-  for(int i = 0; i < nn->nInput+1; i++) {   
-    free(nn->weightIH[i]);
-    free(nn->deltaIH[i]);
-  }
-  free(nn->weightIH);
-  free(nn->deltaIH);
-  for(int i = 0; i < nn->nHidden+1; i++) {   
-    free(nn->weightHO[i]);
-    free(nn->deltaHO[i]);    
-  }
-  free(nn->weightHO);
-  free(nn->deltaHO);
-  free(nn);
 }
 
 void loadWeights(NeuralNetwork* neuralNetwork, const char* inputFile) {
@@ -133,27 +91,27 @@ void feedForward(NeuralNetwork* nn, float* pattern) {
   std::ofstream outfile;
   outfile.open("feedforward.dat", std::ofstream::app);
   // enter pattern value into input neurons
-  for (int j = 0; j < nn->nInput; j++){
+  for (int j = 0; j < INPUTSIZE; j++){
     //outfile << "input[" << j << "] = " << pattern[j] << "\n";
     nn->input[j] = pattern[j];
   }
-  for (int j = 0; j < nn->nHidden; j++) {
+  for (int j = 0; j < HIDDENSIZE; j++) {
     nn->hidden[j] = 0;
     // sum the inputs times the weights between them and the hidden neurons
     // and then enter them into an activation function: 1/(1+exp(-x))
-    for (int k = 0; k < nn->nInput+1; k++)
-      nn->hidden[j] += nn->input[k] * nn->weightIH[k][j];
+    for (int k = 0; k < INPUTSIZE+1; k++)
+      nn->hidden[j] += nn->input[k] * nn->weightIH[k*HIDDENSIZE + j];
     nn->hidden[j] = 1/(1+exp(-1*(nn->hidden[j])));
     //outfile << "hidden[" << j << "] = " << nn->hidden[j] << "\n";
   }
 
   // enter value generated in the hidden layer to the output neurons
-  for (int j = 0; j < nn->nOutput; j++) {
+  for (int j = 0; j < OUTPUTSIZE; j++) {
     nn->output[j] = 0;
     // sum the hidden values times the weights between them and the output neurons
     // and then enter them into an activation function: 1/(1+exp(-x))
-    for (int k = 0; k < nn->nHidden+1; k++)
-      nn->output[j] += nn->hidden[k] * nn->weightHO[k][j];
+    for (int k = 0; k < HIDDENSIZE+1; k++)
+      nn->output[j] += nn->hidden[k] * nn->weightHO[k*OUTPUTSIZE + j];
     nn->output[j] = 1/(1+exp(-1*(nn->output[j])));
     //outfile << "output[" << j << "] = " << nn->output[j] << "\n";
   }
@@ -176,12 +134,12 @@ int guessClassification(int nOutput, float* output) {
 // PRECONDITIONS:
 //   * each individual input MUST be composed of nInput values
 //   * inputs and labels MUST BOTH have 'size' number of elements
-float getTestAccuracy(NeuralNetwork* nn, float** inputs, int* labels, int size) {
+float getTestAccuracy(NeuralNetwork* nn, float* inputs, int* labels, int size) {
   int incorrectPatterns = 0;
 
   for (int i = 0; i < size; i++) {
-    feedForward(nn, inputs[i]);
-    int guess = guessClassification(nn->nOutput, nn->output);
+    feedForward(nn, &inputs[i*INPUTSIZE]);
+    int guess = guessClassification(OUTPUTSIZE, nn->output);
     if (guess != labels[i]) incorrectPatterns++;
   }
   return 1.0 - (float)incorrectPatterns / (float)size;
@@ -193,14 +151,9 @@ float getTestAccuracy(NeuralNetwork* nn, float** inputs, int* labels, int size) 
 //   * each individual input MUST be composed of nInput values
 //   * inputs and labels MUST BOTH have 'size' number of elements
 void trainNetwork( NeuralNetwork* nn, 
-                   float** inputs, int* labels, int size,
-                   float** testInputs, int* testLabels, int testSize) {
+                   float* inputs, int* labels, int size,
+                   float* testInputs, int* testLabels, int testSize) {
   int epoch = 0;
-  float* input = nn->input;
-  float* hidden = nn->hidden;
-  float* output = nn->output;
-  float** weightIH = nn->weightIH;
-  float** weightHO = nn->weightHO;
 
   std::ofstream outfile;
   outfile.open("feedforward.dat");
@@ -218,68 +171,75 @@ void trainNetwork( NeuralNetwork* nn,
       // Feedforward training input
       //***************************************************************************************************************
 
-      feedForward(nn, inputs[i]);
+      feedForward(nn, &inputs[i*INPUTSIZE]);
 
       //***************************************************************************************************************
       // backpropagate errors
       // note: can this be improved with the calculation of error gradients?
       //***************************************************************************************************************
 
-      for (int j = 0; j < nn->nOutput; j++) {
+      for (int j = 0; j < OUTPUTSIZE; j++) {
         float target = labels[i] == j;
         outfile << "label: " << labels[i] << "     j: " << j << "\n";
         // set error gradient for the output node
-        nn->outputErrorGradients[j] = output[j] * (1-output[j]) * (target - output[j]);
+        nn->outputErrorGradients[j] = nn->output[j] * (1-nn->output[j]) * (target - nn->output[j]);
         //outfile << "outputErrorGradients[" << j << "]= " << nn->outputErrorGradients[j] << "\n";
-        for (int k = 0; k < nn->nHidden+1; k++) {
-          nn->deltaHO[k][j] = learningRate * hidden[k] * nn->outputErrorGradients[j] + momentum * nn->deltaHO[k][j];
+        for (int k = 0; k < HIDDENSIZE+1; k++) {
+          nn->deltaHO[k*OUTPUTSIZE + j] = learningRate * nn->hidden[k] * nn->outputErrorGradients[j] + momentum * nn->deltaHO[k*OUTPUTSIZE + j];
           //outfile << "deltaHO[" << k << "][" << j << "= " << nn->deltaHO[k][j] << "\n";
         }
       }
 
-      for (int j = 0; j < nn->nHidden; j++) {
+      for (int j = 0; j < HIDDENSIZE; j++) {
         // set error gradient for the output node based on weightsHO times outputErrorGradients
         float sum = 0;
-        for (int k = 0; k < nn->nOutput; k++) sum+= weightHO[j][k] * nn->outputErrorGradients[k];
-        nn->hiddenErrorGradients[j] = hidden[j] * (1-hidden[j]) * sum;
+        for (int k = 0; k < OUTPUTSIZE; k++) sum+= nn->weightHO[j*OUTPUTSIZE + k] * nn->outputErrorGradients[k];
+        nn->hiddenErrorGradients[j] = nn->hidden[j] * (1-nn->hidden[j]) * sum;
         //outfile << "hiddenErrorGradients[" << j << "]= " << nn->hiddenErrorGradients[j] << "\n";
-        for (int k = 0; k < nn->nInput+1; k++)
-          nn->deltaIH[k][j] = learningRate * input[k] * nn->hiddenErrorGradients[j] + momentum * nn->deltaIH[k][j];
+        for (int k = 0; k < INPUTSIZE+1; k++)
+          nn->deltaIH[k*HIDDENSIZE + j] = learningRate * nn->input[k] * nn->hiddenErrorGradients[j] + momentum * nn->deltaIH[k*HIDDENSIZE + j];
       }
 
       //***************************************************************************************************************
       // update weights
       //***************************************************************************************************************      
-      for (int j = 0; j < nn->nInput+1; j++) {
-        for (int k = 0; k < nn->nHidden; k++) {
-          weightIH[j][k] += nn->deltaIH[j][k];
+      for (int j = 0; j < INPUTSIZE+1; j++) {
+        for (int k = 0; k < HIDDENSIZE; k++) {
+          nn->weightIH[j*HIDDENSIZE + k] += nn->deltaIH[j*HIDDENSIZE + k];
         }
       }
-      for (int j = 0; j < nn->nHidden+1; j++) {
-        for (int k = 0; k < nn->nOutput; k++) {
-          weightHO[j][k] += nn->deltaHO[j][k];
+      for (int j = 0; j < HIDDENSIZE+1; j++) {
+        for (int k = 0; k < OUTPUTSIZE; k++) {
+          nn->weightHO[j*OUTPUTSIZE + k] += nn->deltaHO[j*OUTPUTSIZE + k];
         }
       }
 
       //***************************************************************************************************************
       // check if pattern was correctly identified
       //*************************************************************************************************************** 
-      int guess = guessClassification(nn->nOutput, output);
+      int guess = guessClassification(OUTPUTSIZE, nn->output);
       if (guess != labels[i]) incorrectPatterns++;
 
-      if (i % (size/10) == 0) {
-        nn->trainingSetAccuracy = 1.0 - (float)incorrectPatterns / (float)size * 10.0;
+      // if (i % (size/10) == 0) {
+      //   nn->trainingSetAccuracy = 1.0 - (float)incorrectPatterns / (float)size * 10.0;
 
-        nn->testSetAccuracy = getTestAccuracy(nn, testInputs, testLabels, testSize);
+      //   nn->testSetAccuracy = getTestAccuracy(nn, testInputs, testLabels, testSize);
 
-        std::cout << "Epoch :" << epoch << "\n";
-        std::cout << " Training Set Acc:" << nn->trainingSetAccuracy << "\n";
-        std::cout << " Test Set Acc:" << nn->testSetAccuracy << "\n";
-        incorrectPatterns = 0;
-      }
+      //   std::cout << "Epoch :" << epoch << "\n";
+      //   std::cout << " Training Set Acc:" << nn->trainingSetAccuracy << "\n";
+      //   std::cout << " Test Set Acc:" << nn->testSetAccuracy << "\n";
+      //   incorrectPatterns = 0;
+      // }
     }
-    
 
+    nn->trainingSetAccuracy = 1.0 - (float)incorrectPatterns / (float)size;
+
+    nn->testSetAccuracy = getTestAccuracy(nn, testInputs, testLabels, testSize);
+
+    std::cout << "Epoch :" << epoch << "\n";
+    std::cout << " Training Set Acc:" << nn->trainingSetAccuracy << "\n";
+    std::cout << " Test Set Acc:" << nn->testSetAccuracy << "\n";
+    
     epoch++;
   }
   outfile.close();
