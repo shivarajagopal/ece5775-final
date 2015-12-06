ca59523b7ada735ff000a7f9c6543b7e1017c0d0
tjp79 <tjp79@cornell.edu>
2015-12-06 16:55:52 -0500

    fix tabs

--- audio_lab/fir_test.c
+++ audio_lab/fir_test.c
@@ -14,13 +14,13 @@ int main () {
   fp=fopen("fir_impulse.dat","w");
   int i;
   for (i=0;i<SAMPLES;i++) {
-	  if(i==0)
-		  signal = 0x8000;
-	  else
-		  signal = 0;
-	  fir(&output,signal);
-   	  printf("%i %d %d\n",i,(int)signal,(int)output);
-//   	  fprintf(fp,"%i %d %d\n",i,signal,output);
+    if(i==0)
+      signal = 0x8000;
+    else
+      signal = 0;
+    fir(&output,signal);
+      printf("%i %d %d\n",i,(int)signal,(int)output);
+//      fprintf(fp,"%i %d %d\n",i,signal,output);
   }
   fclose(fp);
   return 0;
