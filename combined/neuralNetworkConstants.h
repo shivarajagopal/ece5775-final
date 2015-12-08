f3e1883da7d564382d03affdbdb2e28a5da1b553
iyv2 <iyv2@cornell.edu>
2015-12-08 09:51:13 -0500

    train using extra data, adjust weights to make it work a little better. Much of the training data still isn't working

--- combined/neuralNetworkConstants.h
+++ combined/neuralNetworkConstants.h
@@ -2,16 +2,15 @@
 #define NEURALNETWORKCONSTANTS_H
 
 #define learningRate 0.001
-#define momentum     0.85
+#define momentum     0.90
 #define maxEpoch     10
 #define INPUT_ROWS 63
 #define INPUT_COLS 14
 #define INPUT_SIZE    882 // = INPUT_ROWS*INPUT_COLS
 #define HIDDEN_SIZE    25
-#define OUTPUT_SIZE    20
-#define OUTPUT_MOD_SIZE 4
-#define DESIRED_ACCURACY 0.99
-#define CLASSIFICATION_THRESHOLD 0.00
+#define OUTPUT_SIZE    4
+#define DESIRED_ACCURACY 0.98
+#define CLASSIFICATION_THRESHOLD 0.70
 
 #endif
 
