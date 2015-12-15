1d7f5a5fb9c6f41f79c2f09a24224bbad6c33c33
zh62 <zh62@cornell.edu>
2015-12-14 19:44:15 -0500

    final version of hardware

--- audio_lab/audio4/audio.sdk/TestApp/Debug/zed_testapp.d
+++ audio_lab/audio4/audio.sdk/TestApp/Debug/zed_testapp.d
@@ -1,6 +1,4 @@
-zed_testapp.o zed_testapp.d: ../zed_testapp.c ../zed_audio.h \
- ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
- ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+zed_testapp.o zed_testapp.d: ../zed_testapp.c \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h \
@@ -8,13 +6,15 @@ zed_testapp.o zed_testapp.d: ../zed_testapp.c ../zed_audio.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/sleep.h \
- ../../standalone_bsp_0/ps7_cortexa9_0/include/xiicps.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../zed_audio.h ../../standalone_bsp_0/ps7_cortexa9_0/include/xiicps.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xiicps_hw.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
- ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xvoicerec.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xvoicerec_hw.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xuartps.h \
@@ -25,12 +25,6 @@ zed_testapp.o zed_testapp.d: ../zed_testapp.c ../zed_audio.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic_hw.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_exception.h \
  ../test_forward2.h ../test_right2.h ../test_left2.h ../test_reverse2.h
-
-../zed_audio.h:
-
-../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
-
-../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
 
@@ -46,7 +40,15 @@ zed_testapp.o zed_testapp.d: ../zed_testapp.c ../zed_audio.h \
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
 
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
+
 ../../standalone_bsp_0/ps7_cortexa9_0/include/sleep.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../zed_audio.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xiicps.h:
 
@@ -57,8 +59,6 @@ zed_testapp.o zed_testapp.d: ../zed_testapp.c ../zed_audio.h \
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xiicps_hw.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
-
-../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xvoicerec.h:
 
